1b4a82c52e499b5dce22f9867318c6cf0ffeeb02
germix <germix@germix.net>
2013-04-17 16:28:33 +0200

    First Working Version

--- README.md
+++ README.md
@@ -1,4 +1,61 @@
-jira-cmd
-========
+# jira-cmd
 
-A Jira Command Line Interface.
+A Jira command line interface based on [jilla](https://github.com/godmodelabs/jilla).
+
+
+## Installation
+
+Install [node.js](http://nodejs.org/).
+
+Then, in your shell type:
+
+	$ npm install -g jira-cmd
+
+
+## Usage
+
+##### First use
+
+	$ jira
+	Jira URL: https://jira.atlassian.com/
+	Username: xxxxxx
+	Password: xxxxxx
+	Information stored!
+
+This save your credentials (base64 encoded) in your `$HOME/.jira` folder.
+
+##### Help
+
+	$ jira [options] [command]
+
+  Commands:
+
+    ls [options]           List my issues
+    start <issue>          Start working on an issue.
+    stop <issue>           Stop working on an issue.
+    running                List issues in progress.
+    search <term>          Find issues.
+    assign <issue>         Assign a issue to me.
+    comment <issue> [text] Comment an issue.
+    show [options] <issue> Show info about an issue
+    config [options]       Change configuration
+
+  Options:
+
+    -h, --help     output usage information
+    -V, --version  output the version number
+
+
+Each command have individual usage help (using --help or -h)
+
+
+## MIT License
+
+Copyright (c) 2013 <germanrcuriel@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
---
+++ bin/jira.js
@@ -0,0 +1,156 @@
+#!/usr/bin/env node
+
+var requirejs = require('requirejs');
+
+requirejs.config({
+  baseUrl: __dirname
+});
+
+requirejs([
+  'commander',
+  '../lib/config',
+  '../lib/auth',
+  '../lib/jira/ls',
+  '../lib/jira/describe',
+  '../lib/jira/assign',
+  '../lib/jira/comment',
+  '../lib/jira/transitions'
+], function (program, config, auth, ls, describe, assign, comment, transitions) {
+
+  program
+    .version('v0.0.5');
+
+  program
+    .command('ls')
+    .description('List my issues')
+    .option('-p, --project <name>', 'Filter by project', String)
+    .option('-t, --type <name>', 'Filter by type', String)
+    .action(function (options) {
+      auth.setConfig(function (auth) {
+        if (auth) {
+          if (options.project) {
+            ls.showByProject(options.project, options.type);
+          } else {
+            ls.showAll(options.type);
+          }
+        }
+      });
+    });
+
+  program
+    .command('start <issue>')
+    .description('Start working on an issue.')
+    .action(function (issue) {
+      auth.setConfig(function (auth) {
+        if (auth) {
+          transitions.start(issue);
+        }
+      });
+    });
+
+  program
+    .command('stop <issue>')
+    .description('Stop working on an issue.')
+    .action(function (issue) {
+      auth.setConfig(function (auth) {
+        if (auth) {
+          transitions.stop(issue);
+        }
+      });
+    });
+
+  program
+    .command('running')
+    .description('List issues in progress.')
+    .action (function () {
+      auth.setConfig(function (auth) {
+        if (auth) {
+          ls.showInProgress();
+        }
+      });
+    });
+
+  program
+    .command('search <term>')
+    .description('Find issues.')
+    .action(function (query) {
+      auth.setConfig(function (auth) {
+        if (auth) {
+          ls.search(query);
+        }
+      });
+    });
+
+
+  program
+    .command('assign <issue>')
+    .description('Assign a issue to me.')
+    .action(function (issue) {
+      auth.setConfig(function (auth) {
+        if (auth) {
+          assign.me(issue);
+        }
+      })
+    });
+
+  program
+    .command('comment <issue> [text]')
+    .description('Comment an issue.')
+    .action(function (issue, text) {
+      auth.setConfig(function (auth) {
+        if (auth) {
+          if (text) {
+            comment.to(issue, text);
+          } else {
+            comment.show(issue);
+          }
+        }
+      });
+    });
+
+  program
+    .command('show <issue>')
+    .description('Show info about an issue')
+    .option('-o, --output <field>', 'Output field content', String)
+    .action(function (issue, options) {
+      auth.setConfig(function (auth) {
+        if (auth) {
+          if (options.output) {
+            describe.show(issue, options.output);
+          } else {
+            describe.show(issue);
+          }
+        }
+      });
+    });
+
+  program
+    .command('config')
+    .description('Change configuration')
+    .option('-c, --clear', 'Clear stored configuration')
+    .action(function (options) {
+      if (options.clear) {
+        auth.clearConfig();
+      } else {
+        auth.setConfig();
+      }
+    }).on('--help', function () {
+      console.log('  Config Help:');
+      console.log();
+      console.log('    Jira URL: https://foo.atlassian.net/');
+      console.log('    Username: user (for user@foo.bar)');
+      console.log('    Password: Your password');
+      console.log();
+    });
+
+  program.parse(process.argv);
+
+  if (program.args.length === 0) {
+    auth.setConfig(function (auth) {
+      if (auth) {
+        program.help();
+      }
+    });
+  }
+
+});
