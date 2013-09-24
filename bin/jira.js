80d5f6883a4c8794d870b06aa5b1ccd9bec2689f
germanrcuriel <germix@germix.net>
2013-09-24 14:22:55 +0200

    Bump version 0.1.0

--- bin/jira.js
+++ bin/jira.js
@@ -19,7 +19,7 @@ requirejs([
 ], function (program, config, auth, ls, describe, assign, comment, create, transitions) {
 
   program
-    .version('v0.0.5');
+    .version('v0.1.0');
 
   program
     .command('ls')
